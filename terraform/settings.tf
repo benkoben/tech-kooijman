023323d4a82bfbbe3ed4b363b92aa987f6c471ed
Ben Kooijman <100195282+benkoben@users.noreply.github.com>
2022-03-03 11:49:43 +0100

    Update settings.tf

--- terraform/settings.tf
+++ terraform/settings.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.97.0"
+    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "2.20.0"
@@ -8,6 +12,12 @@ terraform {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = var.state_store_resource_group
+    storage_account_name = var.state_storage_account
+    container_name       = var.state_contianer_name
+    key                  = var.state_blob_name
   }
 }
 
